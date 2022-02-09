bff465301672050522657d2fbd6e10af4891d343
SanjeevYadavcr7 <sanjeevyadavcr7@gmail.com>
2022-02-09 22:17:44 +0530

    Topic: K Pairs

---
+++ POTD/k_pairs.cc
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int findPairs(vector<int>& nums, int k){
+	unordered_map<int,int> mp;
+	set<pair<int,int>> pairs;
+	
+	for(int& i : nums) mp[i] += 1;
+	for(int& curr_num : nums){
+		mp[curr_num] -= 1;
+		
+		int x = curr_num + k;
+		int y = curr_num - k;
+		
+		if(mp[x] > 0){
+			if(x > curr_num) pairs.insert({curr_num,x});
+			else pairs.insert({x,curr_num});
+		}
+		
+		if(mp[y] > 0){
+			if(y > curr_num) pairs.insert({curr_num,y});
+			else pairs.insert({y,curr_num});
+		}
+		
+		mp[curr_num] += 1;
+	}
+	
+	return pairs.size();
+}
+
+int main(){	
+	int n,k;
+	cin >> n;
+	vector<int> arr(n);
+	for(int i=0; i<n; i++) cin >> arr[i];
+	cin >> k;
+	
+	int no_of_pairs = findPairs(arr,k);
+	cout << no_of_pairs << "\n";
+
+	return 0;
+}
