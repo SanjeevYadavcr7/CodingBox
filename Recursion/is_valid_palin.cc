53380b57e097b99e74e0a61bbd0f3ee91e2cd10c
SanjeevYadavcr7 <sanjeevyadavcr7@gmail.com>
2022-03-02 17:28:05 +0530

    Remove Invalid Parentheses

---
+++ Recursion/is_valid_palin.cc
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int getMinCharsRemove(string str){
+	stack<char> stk;
+	for(int i=0; i<str.size(); i++){
+		if(stk.empty()) stk.push(str[i]);
+		else{
+			if(str[i] == '(') stk.push(str[i]);
+			else if(str[i] == ')'){
+				if(stk.top() == '(') stk.pop();
+				else stk.push(str[i]);
+			}
+		}
+	}
+	return stk.size();
+}
+
+void solve(string str, int rem, unordered_map<string,bool>& mp){
+	if(rem == 0){
+		if(!mp[str]){
+			mp[str] = true;
+			if(getMinCharsRemove(str) == 0) cout << str << "\n";
+		}
+		return;
+	}
+	
+	for(int i=0; i<str.size(); i++){
+		string left = str.substr(0,i);
+		string right = str.substr(i+1);
+		solve(left+right,rem-1,mp);
+	}
+}
+
+void removeInvalidParenthesis(string str){
+	int min_rem = getMinCharsRemove(str);
+	unordered_map<string,bool> mp;
+	solve(str,min_rem,mp);
+}
+
+int main(){
+	string str;
+	cin >> str;
+	
+	removeInvalidParenthesis(str);	
+	
+	return 0;
+}
