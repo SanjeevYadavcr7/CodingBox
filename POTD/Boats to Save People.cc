7df20e9c2daf24ddb8904e89fb7e07dc8bb59a1c
SanjeevYadavcr7 <sanjeevyadavcr7@gmail.com>
2022-03-24 13:25:40 +0530

    Boats to Save People

---
+++ POTD/Boats to Save People.cc
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int numRescueBoats(vector<int>& people, int limit){
+	int n = people.size(), boats = 0;
+	sort(people.begin(), people.end());
+	
+	int i=0, j=n-1;
+	int weight = 0;
+	while(i <= j){
+		// getting total weight in curr boat
+		weight = people[i] + people[j];
+		
+		if(weight <= limit){
+			// if both selected persons can sit in curr boat
+			i++,j--;
+		}
+		else{
+			// if both selected persons can't sit => motallu ko doosri boat mein baghao
+			j--;
+		}
+		
+		// boat used
+		boats++;
+	}	
+	return boats;
+}
+
+int main(){
+	int n,limit;
+	cin >> n;
+	vector<int> people(n);
+	for(int i=0; i<n; i++) cin >> people[i];
+	cin >> limit;
+	
+	int boats = numRescueBoats(people,limit);
+	cout << boats << "\n";
+
+	return 0;
+}
