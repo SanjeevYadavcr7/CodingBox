62ccbc2a56451df53d3a2f68415ca2aae109ee47
SanjeevYadavcr7 <sanjeevyadavcr7@gmail.com>
2022-03-28 19:24:43 +0530

    Next Smaller/Greater

---
+++ Stack_Queue/Next Greater To Left.cc
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void display(vector<int>& arr){
+	for(int& i : arr) cout << i << " ";
+	cout << "\n";
+}
+
+vector<int> getNextGreaterToLeft(vector<int>& arr){
+	int n = arr.size(), i = 0;
+	vector<int> res(n);
+	stack<int> stk;
+	
+	while(i<n){
+		if(stk.empty()){
+			res[i] = -1;
+			stk.push(i++);
+		}
+		else{
+			while(!stk.empty() && arr[i] > arr[stk.top()]) stk.pop();
+			if(stk.empty()){
+				res[i] = -1;
+				stk.push(i++);			
+			}
+			else{
+				res[i] = arr[stk.top()];
+				stk.push(i++);
+			}
+		}
+	}
+	return res;
+}
+
+int main(){
+	int n;
+	cin >> n;
+	vector<int> arr(n);
+	for(int i=0; i<n; i++) cin >> arr[i];
+	
+	vector<int> res = getNextGreaterToLeft(arr);
+	display(res);
+	
+	return 0;
+}
---
+++ Stack_Queue/Next Smaller To Right.cc
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void display(vector<int>& arr){
+	for(int& i : arr) cout << i << "\n";
+}
+
+vector<int> getNextSmallerToRight(vector<int>& arr){
+	int n = arr.size(), i = n-1;
+	vector<int> res(n);
+	stack<int> stk;
+	
+	while(i>=0){
+		if(stk.empty()){
+			res[i] = -1;
+			stk.push(i--);
+		}
+		else{
+			while(!stk.empty() && arr[i] < arr[stk.top()]) stk.pop();
+			if(stk.empty()){
+				res[i] = -1;
+				stk.push(i--);			
+			}
+			else{
+				res[i] = arr[stk.top()];
+				stk.push(i--);
+			}
+		}
+	}
+	return res;
+}
+
+int main(){
+	int n;
+	cin >> n;
+	vector<int> arr(n);
+	for(int i=0; i<n; i++) cin >> arr[i];
+	
+	vector<int> res = getNextSmallerToRight(arr);
+	display(res);
+	
+	return 0;
+}
